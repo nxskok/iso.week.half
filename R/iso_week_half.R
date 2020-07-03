b123669e9c639f7049df6db62fe23dd87b344a16
Ken Butler <ken@swiss-soccer.net>
2020-07-03 18:08:49 -0400

    base_year

--- R/iso_week_half.R
+++ R/iso_week_half.R
@@ -1,6 +1,7 @@
 #' Week number of date, plus a half if midweek
 #'
 #' @param date a date
+#' @param base_year adds 53 weeks for each year since base year
 #'
 #' @return a (double) week number, with extra half if Tuesday, Wednesday or Thursday
 #'
@@ -9,11 +10,10 @@
 #' iso_week_half(as.Date("2020-07-02"))
 #'
 #' @export
-iso_week_half <- function(date) {
+iso_week_half <- function(date, base_year=2019) {
   iw <- lubridate::isoweek(date-lubridate::days(1))
   dow <- lubridate::wday(date, label=T)
-  # ym <- lubridate::year(date)-2019
+  ym <- lubridate::year(date)-base_year
   iw <- ifelse(dow %in% c("Tue", "Wed", "Thu"), iw-0.5, iw)
-  # iw+ym*53
-  iw
+  iw+ym*53
 }
